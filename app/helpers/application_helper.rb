19901ef5b8136de301ec14619be13a05eb9ef681
icecoll <790372365@qq.com>
2016-05-09 18:01:36 +0800

    Cart badge

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
+  #def cart_badge
+   #   if current_cart
+    #   if current_cart.total_count>0
+     #      return "Cart<span class='badge'>2</span>"
+     #  end
+   # end
+  #  return "Cart"
+ # end
 
 end
